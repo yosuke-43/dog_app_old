58c6766f40f838b074ab59d88027de88f228e6db
yosuke <yosuke20sasami@gmail.com>
2024-01-27 14:21:39 +0900

    投稿作成機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_21_163605) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_27_033618) do
   create_table "answers", force: :cascade do |t|
     t.text "content"
     t.integer "question_id", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_21_163605) do
     t.string "title", null: false
     t.text "body", null: false
     t.integer "user_id", null: false
-    t.integer "dog_id", null: false
+    t.integer "dog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["dog_id"], name: "index_boards_on_dog_id"
