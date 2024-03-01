b6691a844ba18c4dec8160cff8a7026994b842a7
yosuke <yosuke20sasami@gmail.com>
2024-03-02 08:24:33 +0900

    会員登録後自動ログイン機能搭載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to login_path, success: '会員登録が完了しました'
+            login(user_params[:email], user_params[:password]) #ユーザー登録後ログインを自動で行う
+            redirect_back_or_to root_path, success: '会員登録が完了しました'
         else
             flash.now[:danger] = '会員登録に失敗しました'
             render :new
